3f577e2194aac0c8b3d80784f8f5808e8177f9f5
sidd8rth <porwal.siddharth25@gmail.com>
2021-06-16 15:59:29 +0530

    level 4 bcrypt encryption and salting

--- app.js
+++ app.js
@@ -3,69 +3,75 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require('mongoose-encryption');
-
+const bcrypt = require('bcrypt');
+const saltRounds = 12;
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
 //Mongoose setup
-mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true, useUnifiedTopology: true});
-const userSchema = new mongoose.Schema({
-    email: String,
-    password: String
+mongoose.connect('mongodb://localhost:27017/userDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
-
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password']  });
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
 
 const User = mongoose.model("User", userSchema);
 
-app.get("/", function(req, res){
-    res.render("home");
+app.get("/", function(req, res) {
+  res.render("home");
 })
 
-app.get("/register", function(req, res){
-    res.render("register");
+app.get("/register", function(req, res) {
+  res.render("register");
 })
 
-app.get("/login", function(req, res){
-    res.render("login");
+app.get("/login", function(req, res) {
+  res.render("login");
 })
 
-app.post('/register', function(req, res){
+app.post('/register', function(req, res) {
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
     const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
+      email: req.body.username,
+      password: hash
     })
-    newUser.save(function(err){
-        if(err){
-            console.log(err);
-        }else{
-            res.render('secrets')
-        }
+    newUser.save(function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render('secrets')
+      }
     });
+  });
 });
 
-app.post("/login", function(req, res){
-    const username = req.body.username;
-    const password = req.body.password;
-    User.findOne({email: username},function(err, foundUser){
-        if(err){
-            console.log(err);
-        }else{
-            if(foundUser){
-                if (foundUser.password === password){
-                    res.render("secrets");
-                }
-            }
+app.post("/login", function(req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+  User.findOne({
+      email: username
+    }, function(err, foundUser) {
+      if (err) {
+        console.log(err);
+      } else {
+        if (foundUser) {
+          bcrypt.compare(req.body.password, foundUser.password, function(err, result) {
+            res.render("secrets");
+          });
         }
+      }
     });
-});
+  });
 
 
 app.listen(3000, function() {
-    console.log("Server started at port 3000");
+  console.log("Server started at port 3000");
 })
